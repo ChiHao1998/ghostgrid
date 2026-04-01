f03467e08343089b00b09d3787a247cb423e58a5
Wong Chi Hao <chihaowong.98@gmail.com>
2026-04-01 16:14:00 +0800

    feat(terraform): :sparkles: adds Terraform config to deploy Vault via Docker
    Introduces infrastructure as code for running HashiCorp Vault in a Docker container.  Enables dynamic Vault configuration using variables for storage, listener, TLS, and UI settings.  Facilitates reproducible local or development Vault environments with configurable options.

---
+++ terraform/vault/main.tf
@@ -0,0 +1,88 @@
+terraform {
+  required_providers {
+    docker = {
+      source = "kreuzwerker/docker"
+    }
+    local = {
+      source = "hashicorp/local"
+    }
+  }
+}
+
+provider "docker" {}
+provider "local" {}
+
+# Write vault-config.json dynamically from variables
+resource "local_file" "vault_config" {
+  filename = "${path.module}/config/vault-config.json"
+  content  = <<EOT
+{
+  "storage": {
+    "file": {
+      "path": "${var.vault_storage_path}"
+    }
+  },
+  "listener": [
+    {
+      "tcp": {
+        "address": "${var.vault_listener_address}",
+        "tls_disable": ${var.vault_tls_disable}
+      }
+    }
+  ],
+  "ui": ${var.vault_ui}
+}
+EOT
+}
+
+resource "docker_image" "vault" {
+  name = "hashicorp/vault:latest"
+}
+
+resource "docker_container" "vault" {
+  name  = "vault"
+  image = docker_image.vault.image_id
+
+  ports {
+    internal = 8200
+    external = 8200
+  }
+
+  # Mount config directory
+  volumes {
+    host_path      = abspath("${path.module}/config")
+    container_path = "/vault/config"
+  }
+
+  # Mount data directory
+  volumes {
+    host_path      = abspath("${path.module}/data")
+    container_path = "/vault/data"
+  }
+
+  capabilities {
+    add = ["IPC_LOCK"]
+  }
+
+  # Run Vault in server mode using the generated config
+  command = ["server"]
+
+  env = [
+    "VAULT_ADDR=${var.vault_addr}",
+    "VAULT_API_ADDR=${var.vault_addr}"
+  ]
+
+  restart = "no"
+
+  depends_on = [local_file.vault_config]
+}
+
+output "vault_container_name" {
+  description = "The name of the Vault container"
+  value       = docker_container.vault.name
+}
+
+output "vault_ui_url" {
+  description = "Vault UI URL"
+  value       = var.vault_addr
+}
